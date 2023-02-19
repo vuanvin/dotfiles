75e69301b0f0aecde56f9f46a75161dfa5fde355
Yuanyin Zhang <isyuanyin@outlook.com>
2023-02-19 23:37:36 +0800

    add NeoVim config

---
+++ NeoVim/init.lua
@@ -0,0 +1,201 @@
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function(use)
+	if not vim.g.vscode then
+		use {'easymotion/vim-easymotion', opt = true}
+		use 'nvim-treesitter/nvim-treesitter'
+		use {
+		  "loctvl842/monokai-pro.nvim",
+		  config = function()
+		    require("monokai-pro").setup()
+		  end
+		}
+
+		use {
+		  'nvim-lualine/lualine.nvim',
+		  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+		}
+
+		use "williamboman/mason.nvim"
+
+		use {
+		  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		   requires = { {'nvim-lua/plenary.nvim'} }
+		}
+	end
+
+	use {'asvetliakov/vim-easymotion', opt = true, as = 'vsc-easymotion'}
+	use 'tpope/vim-surround'
+
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/nvim-cmp'
+
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/vim-vsnip'
+end)
+
+
+if vim.g.vscode then
+	vim.cmd [[packadd vsc-easymotion]]
+	-- Fix bug in vscode-neovim
+	vim.api.nvim_set_keymap("c", "bn", "tabn", {noremap=true})
+	vim.api.nvim_set_keymap("c", "bp", "tabp", {noremap=true})
+
+else
+	vim.cmd [[packadd vim-easymotion]]
+
+	require("monokai-pro").setup({
+	  transparent_background = false,
+	  terminal_colors = true,
+	})
+	vim.cmd [[colorscheme monokai-pro]]
+
+	require('lualine').setup()
+
+	require("mason").setup()
+
+	local builtin = require('telescope.builtin')
+	vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+	vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+	vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+	vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+end
+
+local opt = vim.opt
+
+vim.cmd [[autocmd BufWinEnter ?* silent! loadview]]
+vim.cmd [[autocmd BufWinLeave ?* silent! mkview]]
+opt.clipboard = "unnamed"
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.number = true
+
+vim.g.mapleader = " "
+
+vim.cmd [[map <Leader> <Plug>(easymotion-prefix)]]
+vim.api.nvim_set_keymap("n", ",", "<Plug>(easymotion-bd-w)", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>j", "<Plug>(easymotion-j)", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>k", "<Plug>(easymotion-k)", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>s", "<Plug>(easymotion-overwin-f2)", {noremap=true})
+
+
+local on_attach = function(client, bufnr)
+    vim.g.completion_matching_strategy_list = "['exact', 'substring', 'fuzzy']"
+
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+
+    local function buf_set_option(...)
+        vim.api.nvim_buf_set_option(bufnr, ...)
+    end
+
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<leader>,', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+-- See https://github.com/hrsh7th/nvim-cmp
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
+require('lspconfig').clangd.setup{
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
