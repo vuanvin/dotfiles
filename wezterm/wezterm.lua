242d0b9c5af3144c9914fc4f1d6a853face1fc7d
Yuanyin Zhang <isyuanyin@outlook.com>
2023-03-03 00:26:04 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,8 +10,11 @@ local configs = {
   window_decorations = "RESIZE",
   window_background_opacity = 0.7,
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+  initial_rows = 50,
+  initial_cols = 160,
   check_for_updates = false,
   color_scheme = "MonokaiPro (Gogh)",
+  -- color_scheme = "Gruvbox Dark", -- "MonokaiPro (Gogh)",
   font = wezterm.font_with_fallback({
     "JetBrainsMono NF",
     "FiraCode Nerd Font",
@@ -24,7 +27,6 @@ local configs = {
     saturation = 1.0,
     brightness = 1.0,
   },
-  default_prog = { 'pwsh', '-NoLogo' },
   launch_menu = {},
   leader = { key = '[', mods = 'ALT', timeout_milliseconds = 5000 },
   disable_default_key_bindings = true,
@@ -40,8 +42,10 @@ local configs = {
       action = act.CopyTo 'ClipboardAndPrimarySelection',
     },
   },
+  command_palette_font_size = 14.0,
   -- https://wezfurlong.org/wezterm/config/lua/keyassignment/index.html
   keys = {
+    -- { key = 'p',          mods = 'SHIFT|CTRL',   action = act.ActivateCommandPalette },
     { key = 'n',          mods = 'SHIFT|CTRL',   action = act.SpawnWindow },
     { key = 'h',          mods = 'SHIFT|CTRL',   action = act.Hide },
     { key = ':',          mods = 'ALT|SHIFT',    action = act.ShowLauncher },
@@ -49,14 +53,18 @@ local configs = {
     { key = ']',          mods = 'LEADER',       action = act.ShowLauncher },
     { key = 'q',          mods = 'CMD',          action = act.QuitApplication },
     { key = 'q',          mods = 'LEADER',       action = act.QuitApplication },
-    { key = 'C',          mods = 'CTRL|SHIFT',   action = act.CopyTo 'ClipboardAndPrimarySelection' },
+    { key = 'q',          mods = 'SHIFT|CTRL',   action = act.QuitApplication },
+    { key = 'C',          mods = 'SHIFT|CTRL',   action = act.CopyTo 'ClipboardAndPrimarySelection' },
     { key = 'v',          mods = 'SHIFT|CTRL',   action = act.Paste },
+
+    -- Tab
     { key = 'c',          mods = 'LEADER',       action = act.SpawnTab 'CurrentPaneDomain', },
     { key = 's',          mods = 'LEADER',       action = act.ShowTabNavigator },
-    { key = 'x',          mods = 'LEADER',       action = act.CloseCurrentPane { confirm = true }, },
     { key = 'x',          mods = 'LEADER|SHIFT', action = act.CloseCurrentTab { confirm = true }, },
     { key = 'p',          mods = "LEADER",       action = act.ActivateTabRelative( -1) },
     { key = 'n',          mods = "LEADER",       action = act.ActivateTabRelative(1) },
+    { key = 'p',          mods = "ALT",          action = act.ActivateTabRelative( -1) },
+    { key = 'n',          mods = "ALT",          action = act.ActivateTabRelative(1) },
     { key = '1',          mods = "LEADER",       action = act.ActivateTab(0) },
     { key = '2',          mods = "LEADER",       action = act.ActivateTab(1) },
     { key = '3',          mods = "LEADER",       action = act.ActivateTab(2) },
@@ -66,6 +74,9 @@ local configs = {
     { key = '7',          mods = "LEADER",       action = act.ActivateTab(6) },
     { key = '8',          mods = "LEADER",       action = act.ActivateTab(7) },
     { key = '9',          mods = "LEADER",       action = act.ActivateTab(8) },
+
+    -- Pane
+    { key = 'x',          mods = 'LEADER',       action = act.CloseCurrentPane { confirm = true }, },
     { key = '|',          mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
     { key = '%',          mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
     { key = '-',          mods = 'LEADER',       action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
@@ -124,8 +135,10 @@ local configs = {
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  table.insert(configs.launch_menu, { label = "pwsh", args = { "pwsh.exe", "-NoLogo" } })
-  table.insert(configs.launch_menu, { label = "Nushell", args = { "nu" } })
+  configs.default_prog = { 'pwsh', '-Login', '-NoLogo' }
+
+  table.insert(configs.launch_menu, { label = "pwsh", args = { "pwsh", "-Login", "-NoLogo" } })
+  table.insert(configs.launch_menu, { label = "Nushell", args = { "nu", "-l" } })
 
   -- Enumerate any WSL distributions that are installed and add those to the menu
   local success, wsl_list, wsl_err = wezterm.run_child_process({ "wsl", "-l" })
@@ -151,9 +164,16 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     end
   end
 
+  table.insert(configs.launch_menu, { label = "Git Bash", args = { [[C:\Program Files\Git\bin\bash.exe]] } })
   table.insert(configs.launch_menu, { label = "PowerShell 5", args = { "powershell.exe", "-NoLogo" } })
-else
+elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+  -- configs.default_prog = { 'zsh', '-l' }
+
   table.insert(configs.launch_menu, { label = "bash", args = { "bash", "-l" } })
+  table.insert(configs.launch_menu, { label = "zsh", args = { "zsh", "-l" } })
+else
+  -- configs.default_prog = { 'zsh', '-l' }
+  --
   table.insert(configs.launch_menu, { label = "zsh", args = { "zsh", "-l" } })
 end
 
